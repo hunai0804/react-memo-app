fd7ecbf4d0fe00d5563b92a4f874dc13d6f620ec
hunai0804 <106151522+hunai0804@users.noreply.github.com>
2024-08-24 23:28:15 +0900

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Main.css";
+
+const Main = ({ activeNote, onUpdateNote }) => {
+  if (!activeNote) {
+    return <div className="no-active-note">ノートが選択されていません</div>;
+  }
+
+  const onEditNote = (key, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [key]: value,
+      modDate: Date.now(),
+    });
+  };
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          type="text"
+          id="title"
+          value={activeNote.title}
+          onChange={(e) => onEditNote("title", e.target.value)}
+        />
+        <textarea
+          id="content"
+          placeholder="ノート内容を記入"
+          value={activeNote.content}
+          onChange={(e) => onEditNote("content", e.target.value)}
+        ></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <div className="markdown-preview">{activeNote.content}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
