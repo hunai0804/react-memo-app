0c8cbb7ca9c9b7d010295e329d55d12e75774546
hunai0804 <106151522+hunai0804@users.noreply.github.com>
2024-08-25 13:31:41 +0900

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,4 +27,22 @@ const Sidebar = ({ onAddNote, notes, onDeleteNote, activeNote, setActiveNote }) 
   );
 };
 
+html
+<div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+          <div className="app-sidebar-note" key={note.id} onClick={() => setActiveNote(note.id)}>
+            <div className="sidebar-note-title">
+              <strong>タイトル</strong>
+              <button>削除</button>
+            </div>
+            <p>内容</p>
+            <small>最後の修正日:xx:xx</small>
+          </div>
+      </div>
+    </div>
+
 export default Sidebar;
